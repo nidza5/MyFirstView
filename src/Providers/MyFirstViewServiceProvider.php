9070d188692bcb00f5bc5dd06ac7ce916577a808
Nikola Vasiljević <vnidza91@gmail.com>
2018-05-04 10:13:54 +0200

    Backend plugin

---
+++ src/Providers/MyFirstViewServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+ 
+namespace MyFirstView\Providers;
+ 
+use Plenty\Plugin\ServiceProvider;
+ 
+class MyFirstViewServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+ 
+    }
+}
